b6056f0da98b270b401e41048829ee62743ff071
xJaimeGx <beanieman019@protonmail.com>
2022-04-02 15:27:18 -0500

    add logout

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
